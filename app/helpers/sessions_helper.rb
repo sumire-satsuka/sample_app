2fc7a6d467f1a9f4c8af2f6b7b265bdf049ad004
hogehogehogehoge <hogehogehoge@example.com>
2023-05-21 21:40:45 +0000

    list 9.17

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,8 +12,14 @@ module SessionsHelper
   end
     # 現在ログイン中のユーザーを返す（いる場合）
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
@@ -22,9 +28,18 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   # 現在のユーザーをログアウトする
   def log_out
+    forget(current_user)
     reset_session
     @current_user = nil   # 安全のため
   end
+
 end
