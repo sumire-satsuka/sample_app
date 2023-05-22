ee64de5af6b2924aa19453d8655941a12219b195
hogehogehogehoge <hogehogehoge@example.com>
2023-05-22 21:23:28 +0000

    10.1.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,10 @@ class UsersController < ApplicationController
       render 'new', status: :unprocessable_entity
     end
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
 
     def user_params
